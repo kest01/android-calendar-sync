eb5964022bef57d9813c2d5575f193210eedc018
Konstantin Kharitonov <kkharitonov@luxoft.com>
2020-05-10 12:24:09 +0300

    remove default activity

--- app/src/main/java/ru/kest/calendar/MainActivity.kt
+++ app/src/main/java/ru/kest/calendar/MainActivity.kt
@@ -1,24 +1,28 @@
 package ru.kest.calendar
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.provider.CalendarContract.Calendars
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        Log.i(TAG, "Hello world TAG!")
+        logCalendars()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -34,6 +38,49 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private val EVENT_PROJECTION = arrayOf(
+        Calendars._ID,  // 0
+        Calendars.ACCOUNT_NAME,  // 1
+        Calendars.CALENDAR_DISPLAY_NAME,  // 2
+        Calendars.OWNER_ACCOUNT // 3
+    )
+
+    // The indices for the projection array above.
+    private val PROJECTION_ID_INDEX = 0
+    private val PROJECTION_ACCOUNT_NAME_INDEX = 1
+    private val PROJECTION_DISPLAY_NAME_INDEX = 2
+    private val PROJECTION_OWNER_ACCOUNT_INDEX = 3
+
+    fun logCalendars() {
+        // Run query
+        var cur: Cursor? = null
+        val cr = contentResolver
+        val uri: Uri = Calendars.CONTENT_URI
+
+        // Submit the query and get a Cursor object back.
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_CALENDAR
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.e(TAG, "No permissions to calendar")
+            return
+        }
+        cur = cr.query(uri, EVENT_PROJECTION, null, null, null)
+
+        while (cur!!.moveToNext()) {
+            // Get the field values
+            val calID = cur.getLong(PROJECTION_ID_INDEX)
+            val displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX)
+            val accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX)
+            val ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX)
+
+            Log.i(TAG, "calID: $calID; displayName: $displayName; accountName: $accountName; ownerName: $ownerName")
+
+//            if (accountName == "KKharitonov@luxoft.com")
         }
     }
 }
